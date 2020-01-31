fe563cded5d5cc3a45c41550e79fe0ce70d53760
Rexagon <reide740@gmail.com>
2020-01-31 18:12:06 +0300

    Remove hardcoded logging

--- src/main.rs
+++ src/main.rs
@@ -31,8 +31,6 @@ pub struct Settings {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    std::env::set_var("RUST_LOG", "info");
-
     env_logger::init();
 
     let mut settings = config::Config::default();
